acf34fdea4b87f02263d5e33192b416597e8f6aa
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-11-18 14:17:54 -0300

    Atividade 2

--- 2_opera_variaveis.php
+++ 2_opera_variaveis.php
@@ -1,1 +1,39 @@
-<!-- digitar o programa -->
+<!-- USANDO A FUNÇÃO GET -->
+<!-- GET: método HTML padrão, a requisição
+ é enviada como uma string anexada a URL -->
+
+<!-- Esse programa recebe dois valores pela URL
+  usando o método GET -->
+<!-- http://localhost/php-basico-Nicolly/2_opera_variaveis.php?numero1=100&numero2=30 -->
+
+<?php
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+
+    // Verifica se os valores foram passados corretamente
+    if(isset($numero1) && isset($numero2)){
+
+        // converter para inteiros
+        $numero1 = (int)$numero1;
+        $numero2 = (int)$numero2;
+
+        // SOMAR
+        $soma = $numero1 + $numero2;
+        // SUBTRAIR
+        $subtracao = $numero1 - $numero2;
+        // MULTIPLICAR
+        $multiplicacao = $numero1 * $numero2;
+        // DIVIDIR
+        $divisao = $numero1 / $numero2;
+
+        // Exibir resultados
+        echo "A soma é: $soma <br>";
+        echo "A subtração é: $subtracao <br>";
+        echo "A multiplicação é: $multiplicacao <br>";
+        echo "A divisão é: $divisao <br>";
+
+    } else{
+        echo "ATENÇÃO! Por favor, forneça os valores de numero1 e numero2 pela URL.";
+    }
+
+  ?>

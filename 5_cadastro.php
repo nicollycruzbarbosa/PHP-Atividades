3243ea4b423a1e375a6aa4de5fa644106b1c6908
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-11-19 10:19:32 -0300

    Atividade 5

--- 5_cadastro.php
+++ 5_cadastro.php
@@ -19,8 +19,26 @@
     </form>
     <?php
 
-    // Digitar PHP (1º Aqui)
-    
+    // Verifica se o formulário foi enviado
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // recebe os números enviados para o formulário
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+
+        // Abre o arquivo usuarios.txt para escrita, adicionando dados
+        $arquivo = fopen('usuarios.txt', 'a');
+
+        // Cria uma linha com o nome e a senha separados 
+        $linha = $nome . '; ' . $senha . "\n";
+
+        // escreve a linha
+        fwrite($arquivo, $linha);
+
+        // fechar
+        fclose($arquivo);
+
+        echo "<p>Usuário cadastrado com sucesso!</p>";
+    }
     ?>
 </body>
 </html>

a06549e313e1cf9821f7a787cb3fb077fd46e019
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-11-19 16:27:34 -0300

    Atividade 15C

--- 15c_logout.php
+++ 15c_logout.php
@@ -1,8 +1,11 @@
 <?php
 // Página de logout (15c_logout.php)
+session_start();
 
+// Destroi a sessão do usuário
+session_destroy();
 
-// Digitar PHP (1º Aqui)
+header("Location: 15a_sistema.php");
 
-
+exit();
 ?>

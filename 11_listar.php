29af67927027740c4449742af346c00176c5159d
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-11-19 16:26:55 -0300

    Atividade 11

--- 11_listar.php
+++ 11_listar.php
@@ -1,6 +1,6 @@
 <?php
 // Conecta ao banco de dados
-$servername = "localhost";
+$servername = "localhost:3306";
 $username = "root";
 $password = "";
 $dbname = "exercicio";
@@ -12,10 +12,36 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
+//Consulta os danos da tabela clientes
+$sql = "SELECT id, nome, email FROM clientes";
+$result = $conn->query($sql);
 
-// Digitar PHP + SQL (1º Aqui)
+//Verifica se existem registros e os exibe em formato de tabela
+if ($result->num_rows > 0) {
+    echo "<h2>Lista de clientes cadastrados</h2>";
 
+    //define formato da tabela
+    echo "<table border='2'>";
 
-// Fecha a conexão
+        //define cabeçalho da tabela
+        echo "<tr><th>ID</th><th>Nome</th><th>Email</th></tr>";
+
+        //enquanto tiver danos preenchidos no BD
+        //listar e exibir em formato de tabela
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+
+    echo "</table>";
+
+} else {
+    echo "Nenhum cliente encontrado.";
+}
+
+// Encerra a conexão
 $conn->close();
 ?>

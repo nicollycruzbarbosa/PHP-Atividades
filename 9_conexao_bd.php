4dd29773787f7dc939dad2a7df4a96ace750286c
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-11-19 15:57:28 -0300

    Atividade 9

--- 9_conexao_bd.php
+++ 9_conexao_bd.php
@@ -13,10 +13,29 @@
 
 <?php
 
-// Digitar PHP
+//Alterar de acordo com o número da porta
+//Verificar conexão com o XAMPP
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "exercicio";
+
+try {
+    // Tenta criar uma conexão com o banco de dados
+    //Confere se as informações estão corretas
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verifica se houver algum erro na conexão
+    if ($conn->connect_error) {
+        throw new Exception("Falha na conexão: " . $conn->connect_error);
+    }
+
+    echo "Conexão realizada com sucesso!";
+
+} catch (Exception $e) {
+    //Exibe uma mensagem de error amigável
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+}
 
 ?>
-
-
-
 
